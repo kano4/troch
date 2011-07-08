5eae1ced85f54d22e8935187e30ea115832d3207
Kano Shinji <shinji.warp@gmail.com>
2011-07-08 16:18:33 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Troch::Application.config.secret_token = 'a1f7a7cb140eb1184aead3163059d828ac625f18b06b31cc0da7564a9cc82797a315694a7730878fa16392a4f29d7839f178cc2b65e1704ef4aa5ba4a787ff36'
