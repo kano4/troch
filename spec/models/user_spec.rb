5c94eb93d280736602cf2eadb05ec7f7f4846faa
kano4 <shinji@kano4.com>
2011-07-25 13:26:43 +0900

    install spork and libnotify

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
 describe User do
-  before(:each) do
-    @attr = {
-      :email => "test@example.com",
-      :password => "passwd",
-      :password_confirmation => "passwd"
-    }
-  end
-
-  it "should require an email address" do
-    true
-  end
 end
