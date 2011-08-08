48f9126d70e3f53f3949385625600ebce4ac048b
kano4 <shinji@kano4.com>
2011-08-08 15:32:32 +0900

    modify spec_helper.rb for reloading app/ automatically in spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@ end
 Spork.each_run do
   # This code will be run each time you run your specs.
 
+  load "#{Rails.root}/config/routes.rb"
+  Dir["#{Rails.root}/app/*/*.rb"].each { |f| load f }
 end
 
 # --- Instructions ---
