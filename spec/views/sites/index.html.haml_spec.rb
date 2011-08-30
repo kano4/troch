04ca5feef695ff90bd83980d5749c26666f5337a
kano4 <shinji@kano4.com>
2011-08-30 17:18:32 +0900

    remove test from index.html.haml_spec.rb

--- spec/views/sites/index.html.haml_spec.rb
+++ spec/views/sites/index.html.haml_spec.rb
@@ -1,28 +1,4 @@
 require 'spec_helper'
 
 describe "sites/index.html.haml" do
-  before(:each) do
-    assign(:sites, [
-      stub_model(Site,
-        :name => "Name",
-        :url => "http://example.com",
-        :content => "MyText",
-        :domain_url => "Domain Url",
-        :ssl_url => "Ssl Url"
-      ),
-      stub_model(Site,
-        :name => "Name",
-        :url => "http://example.com",
-        :content => "MyText",
-        :domain_url => "Domain Url",
-        :ssl_url => "Ssl Url"
-      )
-    ])
-  end
-
-  it "renders a list of sites" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-  end
 end
