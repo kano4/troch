2f50edb1f834ea0f96bf47104a098fc84308a057
kano4 <shinji@kano4.com>
2011-09-06 15:58:00 +0900

    set limit of dashboard log

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @watchlogs = WatchLog.find(:all, :order => "updated_at DESC")
+    @watchlogs = WatchLog.find(:all, :limit => 10, :order => "updated_at DESC")
   end
 
 end
