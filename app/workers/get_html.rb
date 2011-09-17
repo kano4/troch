4a2af81741ea646c68629c4c56f0630ec4142e0e
kano4 <shinji@kano4.com>
2011-09-17 17:53:24 +0900

    rename get_html_server to troch_server and add exception to workers/get_html.rb

--- app/workers/get_html.rb
+++ app/workers/get_html.rb
@@ -4,12 +4,21 @@ class GetHtml
 
   def self.perform(site_id)
     site = Site.find(site_id)
-    start_time = Time.now
-    content = get_page_body(site.url)
-    end_time = Time.now
-    response_time = (end_time - start_time) * 1000
-    site.watch_logs.build(:status => 'ok', :content => content, :response_time => response_time)
-    site.save
+    trials = 0
+    begin
+      start_time = Time.now
+      content = get_page_body(site.url)
+      end_time = Time.now
+    rescue
+      trials += 1
+      retry if trials < 3
+      site.watch_logs.build(:status => 'error', :content => "", :response_time => 0)
+      site.save
+    else
+      response_time = (end_time - start_time) * 1000
+      site.watch_logs.build(:status => 'ok', :content => content, :response_time => response_time)
+      site.save
+    end
   end
 end
 
