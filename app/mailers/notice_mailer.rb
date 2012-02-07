f5531e61037ca92211fef40f7540d9aa23725685
kano4 <shinji.warp@gmail.com>
2012-02-08 01:12:32 +0900

    modify notice_mailer

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -4,18 +4,19 @@ class NoticeMailer < ActionMailer::Base
   default :from => @@data["default"]["from"]
 
   def sendmail_alert(user, site, status, diff_html = '')
-    @user = user
-    @site = site
-    @status = status
-    @diff_html = diff_html
-    @date = Time.now.strftime('%Y/%m/%d %H:%M:%S')
+    @user_email = user.email.force_encoding("UTF-8")
+    @site_name = site.name.force_encoding("UTF-8")
+    @site_url = site.url.force_encoding("UTF-8")
+    @status = status.force_encoding("UTF-8")
+    @diff_html = diff_html.encode("UTF-16BE", :invalid => :replace, :undef => :replace, :replace => '?').encode("UTF-8")
+    @date = Time.now.strftime('%Y/%m/%d %H:%M:%S').force_encoding("UTF-8")
 
     mail to: user.email,
          subject: "[Troch]#{@status}:#{@site.name}"
   end
 
   def sendmail_summary(user, domain_sites, ssl_sites)
-    @user = user
+    @user_email = user.email.force_encoding("UTF-8")
     @domain_sites = domain_sites
     @ssl_sites    = ssl_sites
     mail to: user.email,
