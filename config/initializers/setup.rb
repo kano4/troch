c9082bc340d7db442aaa73edbb297fc87e87ace3
kano4 <shinji.warp@gmail.com>
2012-02-21 20:12:27 +0900

    modify setup.rb

--- config/initializers/setup.rb
+++ config/initializers/setup.rb
@@ -5,4 +5,4 @@ rescue
   raise "Please start Redis"
 end
 #`RAILS_ENV="#{ENV['RAILS_ENV']}" #{Rails.root}/script/troch_server restart`
-`bundle exec whenever --update troch --set environment="#{ENV['RAILS_ENV']}"`
+#`bundle exec whenever --update troch --set environment="#{ENV['RAILS_ENV']}"`
