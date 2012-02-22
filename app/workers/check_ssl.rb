32d2682df931ff60f87eb3ee1abf03db6c6a8903
kano4 <shinji.warp@gmail.com>
2012-02-22 13:42:07 +0900

    fix check_ssl

--- app/workers/check_ssl.rb
+++ app/workers/check_ssl.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-require 'date/format'
+require 'date'
 
 class CheckSsl
   @queue = :troch_worker
@@ -11,7 +11,7 @@ class CheckSsl
       begin
         ssl_expired = `echo | openssl s_client -connect #{site.ssl_url}:443 -showcerts | openssl x509 -dates -noout | grep notAfter | cut -c 10-33`
         ary = Date._parse(ssl_expired)
-        site.ssl_expired = Time.local(ary[:year], ary[:mon], ary[:day]).strftime('%Y-%m-%d')
+        site.ssl_expired = Time.local(ary[:year], ary[:mon], ary[:mday]).strftime('%Y-%m-%d')
       rescue
       else
         site.save
