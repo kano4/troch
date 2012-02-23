6a8069732507875548d2441c098dcf8d5addb248
kano4 <shinji.warp@gmail.com>
2012-02-23 16:01:51 +0900

    fix clock.rb for troch_worker

--- clock.rb
+++ clock.rb
@@ -5,7 +5,7 @@ include Clockwork
 
 ENV['RAILS_ENV'] ||= 'production'
 
-`cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/troch_server restart >> log/cron.log 2>> log/error.log`
+`cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/troch_worker restart >> log/cron.log 2>> log/error.log`
 
 every(1.hour, 'site.get_html', :at => ['**:00', '**:15', '**:30', '**:45']) { `cd #{Rails.root} && script/rails runner Site.get_html -e #{ENV['RAILS_ENV']} >> log/cron.log 2>> log/error.log` }
 every(1.day, 'site.check_domain', :at => '08:00') { `cd #{Rails.root} && script/rails runner Site.check_domain -e #{ENV['RAILS_ENV']} >> log/cron.log 2>> log/error.log` }
@@ -14,4 +14,4 @@ every(1.day, 'site.send_summary', :at => '10:00') { `cd #{Rails.root} && script/
 
 every(1.day, 'site.send_summary', :at => '12:00') { `cd #{Rails.root} && script/rails runner WatchLog.delete_old_logs -e #{ENV['RAILS_ENV']} >> log/cron.log 2>> log/error.log` }
 every(1.day, 'log_clear', :at => '12:00') { `cd #{Rails.root} && bundle exec rake log:clear --silent >> log/cron.log 2>> log/error.log` }
-every(1.day, 'restart_worker', :at => '12:00') { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/troch_server restart >> log/cron.log 2>> log/error.log` }
+every(1.day, 'restart_worker', :at => '12:00') { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/troch_worker restart >> log/cron.log 2>> log/error.log` }
