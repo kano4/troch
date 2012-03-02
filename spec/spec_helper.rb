569dfc408ed347419e7d5080f8256b4c112575a9
kano4 <shinji.warp@gmail.com>
2012-03-03 01:54:52 +0900

    modify spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,7 +34,6 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
