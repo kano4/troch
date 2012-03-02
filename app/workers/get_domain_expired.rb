5570465c091c29edf019c0c39617db796e6a916b
kano4 <shinji.warp@gmail.com>
2012-03-03 00:42:17 +0900

    modify get_domain_expired and get_ssl_expired by less access

--- app/workers/get_domain_expired.rb
+++ app/workers/get_domain_expired.rb
@@ -8,16 +8,21 @@ class GetDomainExpired
     site = Site.find(site_id)
 
     unless site.domain_url.blank?
-      trials = 0
-      begin
-        client = Whois.query(site.domain_url)
-      rescue
-        trials += 1
-        retry if trials < 3
+      same_domain_sites = Site.find(:all, :conditions => ['id < ? AND domain_url = ?', site.id, site.domain_url])
+      if same_domain_sites.blank?
+        trials = 0
+        begin
+          client = Whois.query(site.domain_url)
+        rescue
+          trials += 1
+          retry if trials < 3
+        else
+          site.domain_expired = client.expires_on
+        end
       else
-        site.domain_expired = client.expires_on
-        site.save
+        site.domain_expired = same_domain_sites.first.domain_expired
       end
+      site.save
     end
   end
 end
--- app/workers/get_ssl_expired.rb
+++ app/workers/get_ssl_expired.rb
@@ -8,14 +8,18 @@ class GetSslExpired
     site = Site.find(site_id)
 
     unless site.ssl_url.blank?
-      begin
-        ssl_expired = `echo | openssl s_client -connect #{site.ssl_url}:443 -showcerts | openssl x509 -dates -noout | grep notAfter | cut -c 10-33`
-        ary = Date._parse(ssl_expired)
-        site.ssl_expired = Time.local(ary[:year], ary[:mon], ary[:mday]).strftime('%Y-%m-%d')
-      rescue
+      same_ssl_sites = Site.find(:all, :conditions => ['id < ? AND ssl_url = ?', site.id, site.ssl_url])
+      if same_ssl_sites.blank?
+        begin
+          ssl_expired = `echo | openssl s_client -connect #{site.ssl_url}:443 -showcerts | openssl x509 -dates -noout | grep notAfter | cut -c 10-33`
+          ary = Date._parse(ssl_expired)
+          site.ssl_expired = Time.local(ary[:year], ary[:mon], ary[:mday]).strftime('%Y-%m-%d')
+        rescue
+        end
       else
-        site.save
+        site.ssl_expired = same_ssl_sites.first.ssl_expired
       end
+      site.save
     end
   end
 
