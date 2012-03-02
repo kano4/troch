6b4f9146236c229da24d3426acba524d107340fe
kano4 <shinji.warp@gmail.com>
2012-03-02 15:57:46 +0900

    modify clock

--- clock.rb
+++ clock.rb
@@ -11,8 +11,8 @@ every(10.minutes, 'record_time') { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_E
 every(15.minutes, 'site.get_html') { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/rails runner Site.get_html >> log/cron.log 2>> log/error.log` }
 every(1.day, 'site.get_domain_expired',  :at => '06:00') { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/rails runner Site.get_domain_expired >> log/cron.log 2>> log/error.log` }
 every(1.day, 'site.get_ssl_expired',     :at => '07:00') { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/rails runner Site.get_ssl_expired    >> log/cron.log 2>> log/error.log` }
-every(1.day, 'site.get_page_rank', :at => '08:00')       { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/rails runner Site.get_page_rank      >> log/cron.log 2>> log/error.log` }
-every(1.day, 'site.send_summary',  :at => '10:00')       { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/rails runner Site.send_summary       >> log/cron.log 2>> log/error.log` }
+every(1.day, 'site.get_page_rank',       :at => '08:00') { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/rails runner Site.get_page_rank      >> log/cron.log 2>> log/error.log` }
+every(1.day, 'site.send_summary',        :at => '10:00') { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/rails runner Site.send_summary       >> log/cron.log 2>> log/error.log` }
 
 every(1.day, 'watch_log.delete_old_logs', :at => '12:00') { `cd #{Rails.root} && RAILS_ENV=#{ENV['RAILS_ENV']} script/rails runner WatchLog.delete_old_logs >> log/cron.log 2>> log/error.log` }
 every(1.day, 'log_clear', :at => '12:00') { `cd #{Rails.root} && bundle exec rake log:clear --silent >> log/cron.log 2>> log/error.log` }
