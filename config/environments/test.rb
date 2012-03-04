ff4e9ea94aa664e55d77b4e0a49053d36ef50666
kano4 <shinji.warp@gmail.com>
2012-03-04 10:55:57 +0900

    add rails3.2 features into test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,4 +37,11 @@ Troch::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
